8ac1aef26773b4f4b54621b0959b14a0736e9277
Straponessi <l.a.straponessi@gmail.com>
2023-04-15 14:28:55 +0300

    RecycleViewAdapter_Commit

--- app/src/main/java/com/android/musiclub/FriendListFragment.kt
+++ app/src/main/java/com/android/musiclub/FriendListFragment.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.experimental.UseExperimental
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import coil.memory.MemoryCache
+import com.android.musiclub.adapters.FriendListRecyclerAdapter
 import com.android.musiclub.databinding.FragmentFriendListBinding
+import com.android.musiclub.models.UserModel
 import com.google.firebase.FirebaseApiNotAvailableException
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.app
 
@@ -17,6 +27,16 @@ class FriendListFragment : Fragment() {
 
     private var _binding: FragmentFriendListBinding? = null
     private val binding get() = _binding!!
+
+    private val friendListRecyclerAdapter: FriendListRecyclerAdapter by lazy {
+        FriendListRecyclerAdapter()
+    }
+
+    companion object {
+        var currentUser: UserModel? = null
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,20 +49,60 @@ class FriendListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.recycleviewFriendList.apply {
+            adapter = friendListRecyclerAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false)
+        }
+
         if (FirebaseAuth.getInstance().currentUser == null) {
             findNavController().navigate(R.id.action_friend_List_Fragment_to_loginFragment)
         } else {
             fetchCurrentUser()
+            fetchUsers()
         }
 
-        binding.btnLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            findNavController().navigate(R.id.action_friend_List_Fragment_to_loginFragment)
+        friendListRecyclerAdapter.onItemClickListener = { friend ->
+
         }
     }
 
-    private fun fetchCurrentUser() {
+    private  fun fetchUsers() {
+        Firebase.database.reference
+            .child("users")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if ( snapshot.exists()) {
+                        val list = mutableListOf<UserModel>()
+                        snapshot.children.forEach {
+                            val user = it.getValue<UserModel>()
+                            if (user!!.userUid != FirebaseAuth.getInstance().uid) list.add(user)
+                        }
+                        friendListRecyclerAdapter.setData(list)
+                    }
+                }
 
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+    }
+
+    private fun fetchCurrentUser() {
+        val currentUserUid = FirebaseAuth.getInstance().uid
+        Firebase.database.reference
+            .child("/users/$currentUserUid")
+            .addListenerForSingleValueEvent(object  : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        currentUser = snapshot.getValue<UserModel>()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+            })
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/android/musiclub/adapters/FriendListRecyclerAdapter.kt
@@ -0,0 +1,56 @@
+package com.android.musiclub.adapters
+
+import android.content.ClipData.Item
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.android.musiclub.databinding.FragmentRegisterBinding
+import com.android.musiclub.databinding.ItemFriendsBinding
+import com.android.musiclub.models.UserModel
+
+class FriendListRecyclerAdapter : RecyclerView.Adapter<FriendListRecyclerAdapter.CustomHolder>() {
+
+    private val friendList = mutableListOf<UserModel>()
+
+    var onItemClickListener : ((UserModel) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemFriendsBinding.inflate(inflater, parent, false)
+        return  CustomHolder(binding)
+    }
+
+    override fun getItemCount(): Int = friendList.size
+
+    override fun onBindViewHolder(holder: CustomHolder, position: Int) {
+        holder.bind(friendList[position])
+    }
+
+    fun setData(list: List<UserModel>) {
+        friendList.clear()
+        friendList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    inner class CustomHolder(private val binding: ItemFriendsBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: UserModel) {
+
+            binding.apply {
+                if (user.userImageUri != "") {
+                    itemFriendImageview.load(user.userImageUri) {
+                        transformations(CircleCropTransformation())
+                    }
+                }
+
+                itemFriendName.text = user.userName
+
+                root.setOnClickListener {
+                    onItemClickListener?.invoke(user)
+                }
+            }
+        }
+    }
+
+}
