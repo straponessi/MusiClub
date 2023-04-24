e55cc8ee5af900f49af8379ea449f860dd883a8a
Straponessi <l.a.straponessi@gmail.com>
2023-04-24 18:32:52 +0300

    ProfileImage

--- app/src/main/java/com/android/musiclub/AccountFragment.kt
+++ app/src/main/java/com/android/musiclub/AccountFragment.kt
@@ -1,7 +1,9 @@
 package com.android.musiclub
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,7 +19,11 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
+@Suppress("DEPRECATION")
 class AccountFragment : BaseFragment<FragmentAccountBinding>() {
 
     private lateinit var currentUserUid: String
@@ -95,8 +101,80 @@ class AccountFragment : BaseFragment<FragmentAccountBinding>() {
         }
     }
 
-    override fun onBackPressed() {
 
+    private fun cropImage(uri: Uri?) {
+        context?.let {
+            CropImage.activity(uri)
+                .setActivityTitle("Crop Image")
+                .setCropShape(CropImageView.CropShape.RECTANGLE)
+                .setCropMenuCropButtonTitle("Ok")
+                .start(it, this)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val result = CropImage.getActivityResult(data)
+
+        when (requestCode) {
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE ->{
+                result.uri?.let { uri ->
+                    userProfileImageUri = uri
+                    binding.imageProfile.load(userProfileImageUri) {
+                        transformations(CircleCropTransformation())
+                    }
+                }
+            }
+
+            CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE -> {
+                Log.e("TAG", result.error.toString())
+            }
+        }
+    }
+
+    private fun performUpdateDB(userName: String) {
+        val mapUserName = mutableMapOf<String, String>()
+        mapUserName["userName"] = userName
+
+        Firebase.database.reference
+            .child("/users/$currentUserUid")
+            .updateChildren(mapUserName as Map<String, Any>)
+            .addOnCanceledListener {
+                if(userProfileImageUri != null) saveImageToFirebaseStorage()
+                Toast.makeText(requireContext(),
+                    "Successfully updated", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
+    private fun saveImageToFirebaseStorage() {
+        binding.progressbar.visibility = View.VISIBLE
+
+        val ref = Firebase.storage.reference
+            .child("/userProfileImages/$currentUserUid")
+
+        ref.putFile(userProfileImageUri!!)
+            .addOnSuccessListener {
+                ref.downloadUrl.addOnSuccessListener {
+                    val imageUriMap = mutableMapOf<String, String>()
+                    imageUriMap["userImageUri"] = it.toString()
+
+                    Firebase.database.reference
+                        .child("/users/$currentUserUid")
+                        .updateChildren(imageUriMap as Map<String, Any>)
+                        .addOnSuccessListener {
+                            binding.progressbar.visibility = View.GONE
+                            Toast.makeText(requireContext(),
+                                "Successfully updated", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+    }
+
+    override fun onBackPressed() {
+        requireActivity().finish()
     }
 
     override fun onDestroyView() {
